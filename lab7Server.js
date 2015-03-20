c67432caa5025f5cb00acd70b3d08b0d716a00c8
ckbzo <ec2-user@ip-172-31-41-252.us-west-2.compute.internal>
2015-03-20 05:39:11 +0000

    done

---
+++ lab7Server.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var readline = require('readline');
+var ROOT_DIR = 'html';
+
+http.createServer(function (req, res){
+  var urlObj = url.parse(req.url, true, false);
+  
+  if(urlObj.pathname === "/comment") {
+      console.log("comment route");
+
+      if(req.method === "POST") {
+          console.log("POST comment route");
+         
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("Name: "+reqObj.Name);
+        console.log("Comment: "+reqObj.Comment);
+        res.writeHead(200);
+        res.end("");
+        
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+          if(err) throw err;
+          db.collection('comment').insert(reqObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+          });
+        });
+        
+      });
+      }
+      
+      else if(req.method === "GET") {
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+        if(err) throw err;
+        db.collection("comment", function(err, comments){
+          if(err) throw err;
+          comments.find(function(err, items){
+              items.toArray(function(err, itemArr){
+                console.log("Document Array: ");
+                console.log(itemArr);
+                res.writeHead(200);
+              res.end(JSON.stringify(itemArr));
+              });
+          });
+        });
+      });
+      }
+    }
+  
+  else if(urlObj.pathname === "/getcity"){
+    fs.readFile('cities.dat.txt', function (err,data){
+      if(err) throw err;
+      console.log("In get city function");
+      var cities = data.toString().split('\n');
+      var myRegEx = RegExp('^'+urlObj.query['q']);
+      var jsonresult = []
+      for(var i = 0; i < cities.length; i++){
+        var result = cities[i].search(myRegEx);
+        if(result != -1){
+          console.log(cities[i]);
+          jsonresult.push({city: cities[i]});
+        }
+      }
+      console.log(JSON.stringify(jsonresult));
+      res.writeHead(200);
+      res.end(JSON.stringify(jsonresult));
+    });
+  }
+  else{
+    console.log("Hey buddy");
+    fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data){ 
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
+}).listen(80);
